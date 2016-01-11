2c21b2aff063ca12b48d54c8480ff188d047d918
Ubuntu <ubuntu@ip-10-36-45-154.us-west-2.compute.internal>
2016-01-11 20:29:13 +0000

    add .fixtures

--- spec/classes/example_spec.rb
+++ spec/classes/example_spec.rb
@@ -14,10 +14,10 @@ describe 'role_db' do
           let(:params) {{ }}
 
           it { is_expected.to compile.with_all_deps }
-          it { is_expected.to contain_class('role_db') }
+#          it { is_expected.to contain_class('role_db') }
        
-          it { is_expected.to contain_class('profile_base') }
-          it { is_expected.to contain_class('profile_base::rspec_monitor') }
+#          it { is_expected.to contain_class('profile_base') }
+#          it { is_expected.to contain_class('profile_base::rspec_monitor') }
 
         end
       end
