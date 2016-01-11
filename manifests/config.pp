0e14d6897044a1e56dea840fdf11b374e2cd7539
Ubuntu <ubuntu@ip-10-36-45-154.us-west-2.compute.internal>
2016-01-11 21:26:17 +0000

    remove rspec

--- manifests/config.pp
+++
@@ -1,6 +0,0 @@
-# == Class role_db::config
-#
-# This class is called from role_db for service config.
-#
-class role_db::config {
-}
--- manifests/init.pp
+++ manifests/init.pp
@@ -12,5 +12,6 @@ class role_db
   # a role includes one or more profiles and at least a 'base' profile
   include ::profile_base
   # include rspec monitor to make rspec acceptance test available to monitor system
-  include ::profile_base::rspec_monitor
+
+  include ::profile_mysql
 }
