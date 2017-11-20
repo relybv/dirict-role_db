46ccc483ba9f836b7680b627db783786553311bf
P. Gomersbach <p.gomersbach@rely.nl>
2017-11-20 21:41:27 +0000

    disable man classes

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,16 +13,16 @@ class role_db
   include ::profile_base
   # include rspec monitor to make rspec acceptance test available to monitor system
   include ::profile_base::rspec_monitor
-  include ::profile_telegraf
-  include ::profile_telegraf::rspec_monitor
-  include ::profile_beats
-  include ::profile_beats::rspec_monitor
+#  include ::profile_telegraf
+#  include ::profile_telegraf::rspec_monitor
+#  include ::profile_beats
+#  include ::profile_beats::rspec_monitor
   include ::profile_mysql
   include ::profile_mysql::rspec_monitor
   include ::profile_nfs
   include ::profile_nfs::rspec_monitor
-  include ::profile_mcollective
-  include ::profile_mcollective::rspec_monitor
+#  include ::profile_mcollective
+#  include ::profile_mcollective::rspec_monitor
   include ::profile_redis
   include ::profile_redis::rspec_monitor
 }
